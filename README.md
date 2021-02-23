245820c71134e3a438591c448c47af217ad65a01
iamanx17 <pranj.17am@gmail.com>
2021-02-23 13:52:33 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# nodejs
+ my first server using nodejs
---
+++ app.js
@@ -0,0 +1,9 @@
+var https=require('http')
+https.createServer(function(req,response){
+    response.end("Heheh I am learning node js")
+
+
+
+
+}).listen(8000)
+
